a85eff05d10c2e2af4d3017a067c1ea2438c8b98
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-27 15:36:06 -0700

    got chart data displaying

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+import { UserData } from './Data';
+import BarChart from './components/BarChart';
 
 function App() {
+  const [userData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: 'Users Gained',
+      data: UserData.map((data) => data.userGain),
+    }]
+  });
   return (
     <div className="App">
       <nav>
@@ -14,6 +24,9 @@ function App() {
         </Route>
         <Route path="/charts">
           <h1>Chart Library</h1>
+          <BarChart chartData={userData} />
+            
+
         </Route>
       </Switch>
     </div>
