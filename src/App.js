837d3a500872400cd1b6ddbd9fbfbdc40c280691
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-30 14:44:40 -0700

    got css and table finished

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import { useState } from 'react';
 import { UserData } from './Data';
 import BarChart from './components/BarChart';
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import * as emailData from './users.json';
+import 'rsuite-table/dist/css/rsuite-table.css';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
+  
   const [userData] = useState({
     labels: UserData.map((data) => data.year),
     datasets: [{
@@ -21,6 +27,20 @@ function App() {
       <Switch>
         <Route path="/table">
           <h1>Table Libary</h1>
+          <Table data={emailData}>
+            <Column width={100} sortable fixed resizable>
+              <HeaderCell>First Name</HeaderCell>
+              <Cell dataKey="first_name" />
+            </Column>
+
+            
+            <Column width={100} sortable fixed resizable>
+              <HeaderCell>Last Name</HeaderCell>
+              <Cell dataKey="last_name" />
+            </Column>
+
+          </Table>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         </Route>
         <Route path="/charts">
           <h1>Chart Library</h1>
